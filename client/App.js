5c4e8a1c1b3d59539fa71f1a41f649e14f318586
David Grant <davidgrant132@gmail.com>
2017-07-10 17:56:02 +0100

    Complete home page mockup

--- client/App.js
+++ client/App.js
@@ -6,11 +6,15 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Rankr</Text>
-        <Text>#2706</Text>
-        <Image source={require('./img/girl.jpg')} />
-        <Text>7.6<Text>83</Text></Text>
-        <MaterialCommunityIcons name="cards-outline" size={32} />
+        <View style={styles.top}>
+          <Text style={styles.header}>RANKR</Text>
+          <Text style={styles.rankText}>#2706</Text>
+          <Image style={styles.homeImage} source={require('./img/girl.jpg')} />
+          <Text style={styles.scoreText}>7.6<Text style={styles.subScoreText}>83</Text></Text>
+        </View>
+        <View style={styles.bottom}>
+          <MaterialCommunityIcons name="cards-outline" size={36} />
+        </View>
       </View>
     );
   }
@@ -18,9 +22,43 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 25,
+    marginBottom: 10,
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  top: {
+    flex: 2,
+    alignItems: 'center',
+  },
+
+  bottom: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+
+  header: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+
+  rankText: {
+    fontSize: 20,
+  },
+
+  homeImage: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    margin: 20,
+  },
+
+  scoreText: {
+    fontSize: 120,
+  },
+
+  subScoreText: {
+    fontSize: 28,
+  }
 });
