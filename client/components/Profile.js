ebd6d2d3180f29f9d4a1af71fe1be0faa378bcf4
David Grant <davidgrant132@gmail.com>
2017-07-10 22:17:49 +0100

    Started profile page

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons';
 
 export default class Profile extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>Profile</Text>
+        <Image style={styles.profileImage} source={require('../img/girl.jpg')} />
+        <View style={styles.info}>
+          <Text style={styles.name}>Daisy<Text style={styles.age}>, 24</Text></Text>
+          <View style={styles.location}>
+            <EvilIcons style={styles.locationIcon} name="location" size={20} />
+            <Text style={styles.locationName}>Plymouth, UK</Text>
+          </View>
+          <View style={styles.line}></View>
+        </View>
       </View>
     );
   }
@@ -14,15 +22,51 @@ export default class Profile extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 25,
-    marginBottom: 10,
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: 'white',
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
 
-  header: {
-    fontSize: 32,
+  profileImage: {
+    alignItems: 'flex-start',
+    flex: 1,
+  },
+
+  info: {
+    flex: 1,
+    marginLeft: 20,
+  },
+
+  age: {
+    fontSize: 18,
+    fontWeight: 'normal',
+  },
+
+  name: {
+    marginTop: 8,
+    fontSize: 24,
     fontWeight: 'bold',
   },
+
+  location: {
+    flexDirection: 'row',
+    marginTop: 8,
+  },
+
+  locationIcon: {
+    marginLeft: -4,
+  },
+
+  locationName: {
+    marginLeft: 4,
+  },
+
+  line: {
+    marginTop: 6,
+    marginLeft: -20,
+    height: 1,
+    width: 500,
+    backgroundColor: 'black'
+  },
+
 });
