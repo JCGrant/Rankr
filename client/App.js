3348c9827949d164789d8cbfc0f742241f996a1a
JCGrant <jamescolin.grant@gmail.com>
2017-07-10 17:28:44 +0100

    Replace SVG with vector icons

--- client/App.js
+++ client/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
   render() {
@@ -9,7 +10,7 @@ export default class App extends React.Component {
         <Text>#2706</Text>
         <Image source={require('./img/girl.jpg')} />
         <Text>7.6<Text>83</Text></Text>
-        <Image source={require('.img/swipe-icon.svg')} />
+        <MaterialCommunityIcons name="cards-outline" size={32} />
       </View>
     );
   }
